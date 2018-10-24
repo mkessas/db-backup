ff7341faf4b1eb1dcbef1ee797f26b299c52f046
Mourad Kessas <mourad.kessas@9spokes.com>
2018-10-24 16:02:27 +1300

    Implemented Mongo backup

--- mongo.py
+++ mongo.py
@@ -1,6 +1,9 @@
 import db
 import logging
+import time
+import re
 from util import Util
+from s3 import S3
 
 class Mongo(db.Database):
 
@@ -12,18 +15,85 @@ class Mongo(db.Database):
         self.logger.info("Detecting MongoDB Client...")
         Util.run(["mongodump", "--version" ] )
         self.logger.info("mongodump found")
+        Util.run(["mongo", "--version" ] )
+        self.logger.info("mongo found")
 
-    def backup_db(self, name):
-        self.logger.info("Performing MongoDB Backup...")
+
+    def backup_db(self, db):
+        self.logger.info("Backing up database '" + db + "'")
+        
+        tmpfile = "backup-" + db + "-" + time.strftime('%Y%m%d', time.localtime()) + ".sql"
+        start = time.time()
+        cmd = ["mongodump"] + self.conf.get('mongo', 'options').split(" ") + [
+            "-u", self.conf.get('mongo','username'), 
+            "-p", self.conf.get('mongo','password'), 
+            "-h", self.conf.get('mongo', 'hostname'),
+            "--archive=-",
+            "--db",
+            db,
+        ]
+
+        if self.conf.get("mongo", "compress") == "true":
+            tmpfile += ".gz"
+            cmd.append("--gzip")
+
+        stream = Util.stream(cmd)
+
+        aws = S3.stream(self.conf.get("mongo","s3_bucket") + "/mongo/" + tmpfile, stream.stdout)
+        stream.stdout.close()
+        
+        err = aws.communicate()[1]
+
+        if err == '':
+            self.logger.info("Backup of '" + db + "' completed in " + str(time.time() - start) + " seconds")
+            return None
+        else:
+            return err.strip()
 
     def backup_all_dbs(self):
-        self.logger.info("Backing up all databases")
-        pass
+
+        dbs, err = self.get_db_names()
+
+        if err != '': 
+            return err.rstrip()
+
+        for db in dbs:
+            err = self.backup_db(db)
+            if err != None:
+                self.logger.error(err.strip())
+                continue
+
 
     def get_db_names(self):
-        self.logger.info("Getting list of databases")
-        return ""
 
+        dbs = []
+
+        self.logger.info("Retrieving list of databases...")
+        
+        echo = Util.stream(["echo", "'show dbs'"])
+        stream = Util.stream([
+            "mongo", 
+            "-u", self.conf.get('mongo','username'),
+            "-p", self.conf.get('mongo','password'),
+            self.conf.get('mongo','hostname'),
+            self.conf.get('mongo','options')
+        ], echo.stdout)
+        echo.stdout.close()
+        
+        data, err = stream.communicate()
+
+        if err != '':
+            return None, err
+
+        for name in data.split('\n'):
+            name = name.strip()
+            match = re.match(r'^(\w+)\s+\d+\.\d+.B$', name)
+            if match:
+                dbs.append(match.group(1))
+
+        self.logger.debug("Database list: " + ",".join(dbs))
+        
+        return dbs, err
+        
     def cleanup(self):
         pass
-
