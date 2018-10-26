e4153de7755cfe2ab566528af1b42b720e86af64
Mourad Kessas <mourad.kessas@9spokes.com>
2018-10-26 17:44:00 +1300

    Updated S3 bucket test by creating last_updated.txt file

--- src/s3.py
+++ src/s3.py
@@ -5,7 +5,12 @@ class S3():
 
 
     def check_bucket(self, bucket):
-        return Util.run(["aws", "s3", "ls", "s3://" + bucket])
+        Util.stream(["aws", "s3", "ls", "s3://" + bucket])
+        date = Util.stream(["date", "+%Y%m%d%H%M%S"])
+        stream = Util.stream(["aws", "s3", "cp", "-", "s3://" + bucket + "/last_updated.txt"], date.stdout)
+        date.stdout.close()
+
+        return stream.communicate()
 
     @staticmethod
     def stream(bucket, stdin):
